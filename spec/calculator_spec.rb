0cee456b08a53cab816769abd534a2f156cf379d
Kelsey Thompson <kelsthompson2@gmail.com>
2021-07-20 12:33:39 -0400

    Add stub to two tests

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -17,15 +17,17 @@ RSpec.describe Calculator do
 
   it "can add two random numbers" do
     calc = Calculator.new
-
+    allow(calc).to receive(:get_num).and_return(5)
     expect(calc.add_nums).to be_a(Integer)
     expect(calc.add_nums).to be_between(0, 200)
+    expect(calc.add_nums).to eq(10)
   end
 
   it "can square a random number" do
     calc = Calculator.new
-
+    allow(calc).to receive(:get_num).and_return(50)
     expect(calc.square_num).to be_a(Integer)
     expect(calc.square_num).to be_between(0, 10_000)
+    expect(calc.square_num).to eq(2500)
   end
 end
